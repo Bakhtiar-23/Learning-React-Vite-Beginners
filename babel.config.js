2310c1ab0f780236d78e12fc9b6d246c8479f05e
Bakhtiar Ismail <bakhtiar.khiderismail@edu.turkuamk.fi>
2025-05-15 01:47:51 +0300

    Update CI/CD

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-// babel.config.mjs
-export default {
-    presets: [
-      ['@babel/preset-env', { targets: { node: 'current' } }],
-      ['@babel/preset-react', { runtime: 'automatic' }]
-    ]
-  };
