6c46904aab20d43ece5ba9a53234e05f6052f5f5
Bakhtiar Ismail <bakhtiar.khiderismail@edu.turkuamk.fi>
2025-05-15 11:10:32 +0300

    Deploy to the Github pages

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,10 @@ import { dirname, resolve } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/<learn-react-vite-platform>/',
+  base: "/learn-react-vite-platform",
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
